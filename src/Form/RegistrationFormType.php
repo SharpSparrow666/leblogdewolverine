9a0181b5ac6957dccb43aa47b3bcdfc50dfb72da
DrunkSparrow <a@a.fr>
2023-06-07 15:26:21 +0200

    Ajout système inscription connextion et déconnection

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse Email',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Merci de renseigner une adresse email',
+                    ]),
+                    new Email([
+                        'message' => 'L\'adresse mail {{ value }} n\'est pas une adresse valide',
+                    ]),
+                ],
+            ])
+
+            ->add('plainPassword', RepeatedType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'type' => PasswordType::class,
+                'invalid_message' => 'Le mot de passe ne correspond pas à sa confirmation',
+                'first_options' => [
+                    'label' => 'Mot de passe',
+                ],
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Merci de renseigner un MDP',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre MDP doit contenir au moins {{ limit }} caractères',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                    new Regex([
+                        'pattern' => '/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[ !"#\$%&\'()*+,\-.\/:;<=>?@[\\\\\]\^_`{\|}~]).{8,4096}$/u',
+                        'message' => 'Votre mot de passe doit contenir obligatoirement une minuscule, une majuscule, un chiffre et un caractère spécial',
+                    ]),
+                ],
+            ])
+
+            ->add('pseudonym', TextType::class, [
+                'label' => 'Pseudonym',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Merci de renseigner un pseudonyme',
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'max' => 40,
+                        'minMessage' => 'Votre pseudonyme doit contenir au moins {{ limit }} caractères',
+                        'maxMessage' => 'Votre pseudonyme doit contenir au maximum {{ limit }} caractères',
+                    ]),
+                ],
+            ])
+
+            ->add('save', SubmitType::class, [
+                'label' => 'Créer mon compte',
+                'attr' => [
+                    'class' => 'btn btn-outline-warning w-100',
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+            // TODO : à enlever quand le form sera terminé
+            'attr' => [
+                'novalidate' => 'novalidate',
+            ],
+        ]);
+    }
+}
