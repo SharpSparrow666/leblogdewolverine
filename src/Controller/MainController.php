b24b81ec6c601e4944c6ba5e87a6f97fea14bb34
DrunkSparrow <a@a.fr>
2023-06-07 10:00:30 +0200

    mise en place squelette front + ajout PA

---
+++ src/Controller/MainController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use mysql_xdevapi\RowResult;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * Contrôleur de la page d'accueil
+     */
+
+    #[Route('/', name: 'main_home')]
+    public function home(): Response
+    {
+        return $this->render('main/home.html.twig');
+    }
+}
+
+
