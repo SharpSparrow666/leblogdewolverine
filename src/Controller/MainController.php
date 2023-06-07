78b23e049f7cb8270e9beb625698f7cbc4b00161
DrunkSparrow <a@a.fr>
2023-06-07 17:00:56 +0200

    Ajout page de profil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,18 +6,29 @@ use mysql_xdevapi\RowResult;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class MainController extends AbstractController
 {
-    /**
-     * Contrôleur de la page d'accueil
-     */
 
     #[Route('/', name: 'main_home')]
     public function home(): Response
     {
         return $this->render('main/home.html.twig');
     }
+
+    /**
+     * Contrôleur de la page de profil (réservé aux personnes connectées
+     */
+
+    #[Route('/mon-profil/', name: 'main_profil')]
+    #[IsGranted('ROLE_USER')]
+    public function profil(): Response
+    {
+        return $this->render('main/profil.html.twig');
+    }
+
 }
+
 
 
