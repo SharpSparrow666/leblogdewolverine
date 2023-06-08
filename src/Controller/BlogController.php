7dac3cc5b2b3a236fc9dfdff1b55ff7e15f91c8c
DrunkSparrow <a@a.fr>
2023-06-08 15:04:40 +0200

    Ajout page liste des articles

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -34,29 +34,42 @@ class BlogController extends AbstractController
         $form->handleRequest($request);
 
         // Si le formulaire sans erreur
-        if ($form->isValid() && $form->isSubmitted())
+        if ($form->isSubmitted() && $form->isValid()) {
 
             // Hydratation
             $newArticle
                 ->setPublicationDate(new \DateTime())
                 ->setAuthor($this->getUser());
 
-        //Sauvegarde en BDD grâce au manager des entités
-        $em = $doctrine->getManager();
-        $em->persist($newArticle);
-        $em->flush();
+            //Sauvegarde en BDD grâce au manager des entités
+            $em = $doctrine->getManager();
+            $em->persist($newArticle);
+            $em->flush();
 
-        //Message flash de succès
-        $this->addFlash('success', 'Article publié avec succès !');
+            //Message flash de succès
+            $this->addFlash('success', 'Article publié avec succès !');
 
-        // TODO : Penser à rediriger sur la page qui montre le nouvel article
-        return $this->redirectToRoute('main_home');
+            // TODO : Penser à rediriger sur la page qui montre le nouvel article
+            return $this->redirectToRoute('main_home');
+        }
 
+        return $this->render('blog/new_publication.html.twig', [
+            'new_publication_form' => $form->createView(),
+        ]);
     }
 
-        return $this->render('blog/new_publication.html.twig',[
-            'new_publication_form' =>$form->createView(),
-            ]);
-    }
+    // Contrôleur de la page qui liste les articles
 
+    #[Route('/publications/liste/', name: 'publication_list')]
+    public function publicationList(ManagerRegistry $doctrine): Response
+    {
+
+        $articleRepo = $doctrine->getRepository(Article::class);
+
+        $articles = $articleRepo->findAll();
+
+        return $this->render('blog/publication_list.html.twig', [
+            'articles' => $articles,
+        ]);
+    }
 }
--- src/Form/NewPublicationFormType.php
+++ src/Form/NewPublicationFormType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use mysql_xdevapi\Warning;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -67,3 +68,6 @@ class NewPublicationFormType extends AbstractType
       ]);
     }
 }
+
+
+
