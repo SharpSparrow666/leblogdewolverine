ee6dae939a043f2edf16e827bc32714e3b8677c9
DrunkSparrow <a@a.fr>
2023-06-08 11:03:02 +0200

    Ajout page création d'article

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Form\NewPublicationFormType;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+/**
+ * Préfixe de la route et du nom de toutes les pages de la partie blog du site
+ */
+#[Route('/blog', name: 'blog_')]
+class BlogController extends AbstractController
+{
+
+    /**
+     * Contrôleur de la page permettant de créer un nouvel article
+     */
+    #[Route('/nouvelle-publication/', name: 'new_publication')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function newPublication(Request $request, ManagerRegistry $doctrine): Response
+    {
+        // Création d'un nouvel Article vide
+        $newArticle = new Article();
+
+        $form = $this->createForm(NewPublicationFormType::class, $newArticle);
+
+        //Liaison des données post au formulaire
+        $form->handleRequest($request);
+
+        // Si le formulaire sans erreur
+        if ($form->isValid() && $form->isSubmitted())
+
+            // Hydratation
+            $newArticle
+                ->setPublicationDate(new \DateTime())
+                ->setAuthor($this->getUser());
+
+        //Sauvegarde en BDD grâce au manager des entités
+        $em = $doctrine->getManager();
+        $em->persist($newArticle);
+        $em->flush();
+
+        //Message flash de succès
+        $this->addFlash('success', 'Article publié avec succès !');
+
+        // TODO : Penser à rediriger sur la page qui montre le nouvel article
+        return $this->redirectToRoute('main_home');
+
+    }
+
+        return $this->render('blog/new_publication.html.twig',[
+            'new_publication_form' =>$form->createView(),
+            ]);
+    }
+
+}
