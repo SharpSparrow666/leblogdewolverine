598650171ee2185d9f97b3bcfa14ccf93ab95b42
DrunkSparrow <a@a.fr>
2023-06-13 16:44:32 +0200

    Ajout upload photo de profil + optimisation images

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Form\EditPhotoFormType;
+use Doctrine\Persistence\ManagerRegistry;
+use Liip\ImagineBundle\Imagine\Cache\CacheManager;
 use mysql_xdevapi\RowResult;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -26,6 +30,52 @@ class MainController extends AbstractController
     public function profil(): Response
     {
         return $this->render('main/profil.html.twig');
+    }
+
+//    Contrôleur de la page de modification de la photo de profil
+//        Accès réservé aux utilisateurs connectés (ROLE_USER)
+
+    #[Route('/changer-photo-de-profil/', name: 'main_edit_photo')]
+    #[IsGranted('ROLE_USER')]
+    public function editPhoto(Request $request, ManagerRegistry $doctrine, CacheManager $cacheManager): Response
+    {
+
+        $form = $this->createForm(EditPhotoFormType::class);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+        $photo = $form->get('photo')->getData();
+
+        $connectedUser = $this->getUser();
+
+        $photoLocation = $this->getParameter('app.user.photo.directory');
+
+        $newFileName = 'user' . $connectedUser->getId() . '.' . $photo->guessExtension();
+
+        if($connectedUser->getPhoto() != null && file_exists($photoLocation . $connectedUser->getPhoto())){
+            $cacheManager->remove('images/profils/' . $connectedUser->getPhoto());
+            unlink($photoLocation . $connectedUser->getPhoto());
+        }
+
+        $em = $doctrine->getManager();
+        $connectedUser->setPhoto($newFileName);
+        $em->flush();
+
+        $photo->move(
+            $photoLocation,
+            $newFileName,
+        );
+
+        $this->addFlash('success', 'Photo de profil modifiée avec succès !');
+        return $this->redirectToRoute('main_profil');
+
+        }
+
+        return $this->render('main/edit_photo.html.twig', [
+            'edit_photo_form' => $form->createView(),
+        ]);
     }
 
 }
